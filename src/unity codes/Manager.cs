2f2fbfd5bcf2b3a4d008aca6175b2b6adaf6b44b
Nikhil-Choudhary <46837375+Nikhil-Choudhary@users.noreply.github.com>
2019-03-27 02:34:15 +0530

    Add files via upload

---
+++ src/unity codes/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    InputController inputController;
+    bool waitState = true;
+    public GameObject cube;
+
+    void Start()
+    {
+        //This will do the network stuff
+        inputController = new InputController();
+        inputController.Begin("192.168.43.188", 80);
+
+    }
+
+    void Update()
+    {
+        
+
+    }
+
+}
